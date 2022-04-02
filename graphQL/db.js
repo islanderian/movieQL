2da099085d32e1116191b2fa51b98f4008e62b5f
islanderian <createdby.island@gmail.com>
2022-04-02 18:35:24 +0900

    #0.7 - Extending the Schema part Two

---
+++ graphQL/db.js
@@ -0,0 +1,19 @@
+export const people = [
+  {
+    id: 1,
+    name: "Nicolas",
+    age: 18,
+    gender: "female",
+  },
+  {
+    id: 2,
+    name: "Jini",
+    age: 36,
+    gender: "male",
+  },
+];
+
+export const getById = (id) => {
+  const filteredPeople = people.filter((person) => people.id === id);
+  return filteredPeople[0];
+};
