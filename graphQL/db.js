3bf0cffd4a5a2db2c161fbf37ff77a9e801864df
islanderian <createdby.island@gmail.com>
2022-04-03 01:09:38 +0900

    #0.13 - Wrapping a REST API with GraphQL Part Two

--- graphQL/db.js
+++ graphQL/db.js
@@ -1,45 +1,18 @@
-let movies = [
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8,
-  },
-  {
-    id: 2,
-    name: "The Godfather",
-    score: 99,
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2,
-  },
-];
+import fetch, { Request } from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  return filteredMovies[0];
-};
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
 
-//--- Mutations
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length + 1,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
