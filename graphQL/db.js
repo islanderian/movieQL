2f054e6876b69da1326b8a58ddb698bdaeef3b5e
islanderian <createdby.island@gmail.com>
2022-04-02 23:04:42 +0900

    #0.9 - Defining Mutations

--- graphQL/db.js
+++ graphQL/db.js
@@ -1,4 +1,4 @@
-export const people = [
+/* export const people = [
   {
     id: 1,
     name: "Nicolas",
@@ -11,9 +11,39 @@ export const people = [
     age: 36,
     gender: "male",
   },
+]; */
+
+const movies = [
+  {
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8,
+  },
+  {
+    id: 2,
+    name: "The Godfather",
+    score: 99,
+  },
+  {
+    id: 3,
+    name: "Logan",
+    score: 2,
+  },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
